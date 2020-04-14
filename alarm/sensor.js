be2aff88b3a8fcabb469270c461058dc0514036e
Bertrand C <bertrand.ciroux@gmail.com>
2020-04-14 17:54:53 +0200

    Formatting code

--- alarm/sensor.js
+++ alarm/sensor.js
@@ -1,71 +1,71 @@
-'use strict';
+"use strict";
 
 //const async = require('async');
 
-module.exports = function(RED) {
+module.exports = function (RED) {
+  function AnamicoAlarmSensor(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
 
-    function AnamicoAlarmSensor(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
+    this._panel = RED.nodes.getNode(config.panel);
+    this.alarmStates = JSON.parse("[" + config.alarmStates + "]");
 
-        this._panel = RED.nodes.getNode(config.panel);
-        this.alarmStates = JSON.parse("[" + config.alarmStates + "]");
+    this.resetTimer = null;
 
-        this.resetTimer = null;
+    node.on("input", function (msg) {
+      //node.log(node.alarmStates);
 
-        node.on('input', function(msg) {
+      node.status({ fill: "blue", shape: "dot", text: "trigger" });
 
-            //node.log(node.alarmStates);
+      msg.payload = {
+        zone: "test",
+        modes: node.alarmStates,
+      };
 
-            node.status({ fill:"blue", shape:"dot", text:"trigger" });
+      if (node.resetTimer) {
+        clearTimeout(node.resetTimer);
+      }
+      node.resetTimer = setTimeout(function () {
+        node.status({});
+        // todo: allow for momentary and fixed (motion vs nc/no)
+      }, 3000);
 
-            msg.payload = {
-                zone: "test",
-                modes: node.alarmStates
-            };
-
+      node._panel &&
+        node._panel.sensor(msg, function (triggered) {
+          if (triggered) {
+            node.log("triggered:" + triggered);
             if (node.resetTimer) {
-                clearTimeout(node.resetTimer);
+              clearTimeout(node.resetTimer);
+              node.resetTimer = null;
             }
-            node.resetTimer = setTimeout(function() {
-                node.status({});
-                // todo: allow for momentary and fixed (motion vs nc/no)
-            }, 3000);
-
-            node._panel && node._panel.sensor(msg, function(triggered) {
-                if (triggered) {
-                    node.log('triggered:' + triggered);
-                    if (node.resetTimer) {
-                        clearTimeout(node.resetTimer);
-                        node.resetTimer = null;
-                    }
-                    return node.status({ fill:"red", shape:"dot", text:"ALARM!" });
-                }
-            });
+            return node.status({ fill: "red", shape: "dot", text: "ALARM!" });
+          }
         });
+    });
 
-        // todo: persist the sensor state on the panel and check it/warn/alarm if alarm state when arming the panel
+    // todo: persist the sensor state on the panel and check it/warn/alarm if alarm state when arming the panel
 
-        /**
-         * listen for panel state changes
-         */
-        node._panel && node._panel.registerStateListener(node, function(msg) {
-            //
-            // alarm state
-            //
-            const SecuritySystemCurrentState = msg.payload && msg.payload.SecuritySystemCurrentState;
-            if (SecuritySystemCurrentState !== 4) {
-                node.status({});
-            }
-        });
+    /**
+     * listen for panel state changes
+     */
+    node._panel &&
+      node._panel.registerStateListener(node, function (msg) {
+        //
+        // alarm state
+        //
+        const SecuritySystemCurrentState =
+          msg.payload && msg.payload.SecuritySystemCurrentState;
+        if (SecuritySystemCurrentState !== 4) {
+          node.status({});
+        }
+      });
 
-        /**
-         * clean up on node removal
-         */
-        node.on('close', function() {
-            node._panel && node._panel.deregisterStateListener(node);
-        });
-
-    }
-    RED.nodes.registerType("AnamicoAlarmSensor", AnamicoAlarmSensor);
+    /**
+     * clean up on node removal
+     */
+    node.on("close", function () {
+      node._panel && node._panel.deregisterStateListener(node);
+    });
+  }
+  RED.nodes.registerType("AnamicoAlarmSensor", AnamicoAlarmSensor);
 };
--- alarm/stateChanged.js
+++ alarm/stateChanged.js
@@ -1,68 +1,72 @@
-'use strict';
+"use strict";
 
 const Formats = {
-    default:    0,
-    homekit:    1,
-    value:      2
+  default: 0,
+  homekit: 1,
+  value: 2,
 };
 
-module.exports = function(RED) {
+module.exports = function (RED) {
+  function AnamicoAlarmStateChanged(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
 
-    function AnamicoAlarmStateChanged(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
+    this.format = parseInt(config.format || "0");
+    this._panel = RED.nodes.getNode(config.panel);
 
-        this.format = parseInt(config.format || '0');
-        this._panel = RED.nodes.getNode(config.panel);
+    /**
+     * listen for panel state changes
+     */
+    this._panel &&
+      this._panel.registerStateListener(this, function (msg) {
+        node.log("new State");
+        node.log(JSON.stringify(msg, null, 2));
 
-        /**
-         * listen for panel state changes
-         */
-        this._panel && this._panel.registerStateListener(this, function(msg) {
-
-            node.log("new State");
-            node.log(JSON.stringify(msg, null, 2));
-
-            node.status({
-                fill: node._panel.isAlarm ? "red" : "green",
-                shape:"dot",
-                text:node._panel.alarmModes[node._panel.alarmState]
-            });
-
-            if (msg.initialState && !config.sendInitialState) {
-                return;
-            }
-            if (msg.payload) {
-                switch (node.format) {
-                    case Formats.homekit:
-                        const oldPayload = msg.payload;
-                        msg.payload = {};
-                        if (typeof oldPayload.SecuritySystemTargetState !== "undefined") {
-                            msg.payload.SecuritySystemTargetState = oldPayload.SecuritySystemTargetState;
-                        }
-                        if (typeof oldPayload.SecuritySystemCurrentState !== "undefined") {
-                            msg.payload.SecuritySystemCurrentState = oldPayload.SecuritySystemCurrentState;
-                        }
-                        if (typeof oldPayload.SecuritySystemAlarmType !== "undefined") {
-                            msg.payload.SecuritySystemAlarmType = oldPayload.SecuritySystemAlarmType;
-                        }
-                        break;
-                    case Formats.value:
-                        msg.payload = msg.payload.SecuritySystemCurrentState;
-                        break;
-                    default:
-                }
-            }
-
-            node.send(msg);
+        node.status({
+          fill: node._panel.isAlarm ? "red" : "green",
+          shape: "dot",
+          text: node._panel.alarmModes[node._panel.alarmState],
         });
 
-        /**
-         * clean up on node removal
-         */
-        node.on('close', function() {
-            node._panel && node._panel.deregisterStateListener(node);
-        });
-    }
-    RED.nodes.registerType("AnamicoAlarmStateChanged", AnamicoAlarmStateChanged);
+        if (msg.initialState && !config.sendInitialState) {
+          return;
+        }
+        if (msg.payload) {
+          switch (node.format) {
+            case Formats.homekit:
+              const oldPayload = msg.payload;
+              msg.payload = {};
+              if (typeof oldPayload.SecuritySystemTargetState !== "undefined") {
+                msg.payload.SecuritySystemTargetState =
+                  oldPayload.SecuritySystemTargetState;
+              }
+              if (
+                typeof oldPayload.SecuritySystemCurrentState !== "undefined"
+              ) {
+                msg.payload.SecuritySystemCurrentState =
+                  oldPayload.SecuritySystemCurrentState;
+              }
+              if (typeof oldPayload.SecuritySystemAlarmType !== "undefined") {
+                msg.payload.SecuritySystemAlarmType =
+                  oldPayload.SecuritySystemAlarmType;
+              }
+              break;
+            case Formats.value:
+              msg.payload = msg.payload.SecuritySystemCurrentState;
+              break;
+            default:
+          }
+        }
+
+        node.send(msg);
+      });
+
+    /**
+     * clean up on node removal
+     */
+    node.on("close", function () {
+      node._panel && node._panel.deregisterStateListener(node);
+    });
+  }
+  RED.nodes.registerType("AnamicoAlarmStateChanged", AnamicoAlarmStateChanged);
 };
